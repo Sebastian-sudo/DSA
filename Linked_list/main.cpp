7bad8695509e761b3b42eab51a718dd6f97fe310
Sebastian Mazurek <smazurek.sm@gmail.com>
2021-02-24 10:24:24 +0100

    added svm ,tree and linked list

---
+++ Linked_list/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+
+struct Node {
+	int item;
+	struct Node* next;
+};
+
+class Linked_list {
+	
+}
+
+int main(int argc, char **argv) {
+
+	return 0;
+}
---
+++ Tree/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct Node {
+	int data*;
+}
+
+class Tree {
+
+};
+
+int main(int argc, char **argv) {
+
+	return 0;
+}
